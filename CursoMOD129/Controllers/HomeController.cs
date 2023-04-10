b4abe3458e9803605f29a7993e6f48ca7c9791b1
lyudinha86 <lyudinha712@gmail.com>
2023-04-10 19:35:20 +0100

    Adicionar arquivos de projeto.

---
+++ CursoMOD129/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CursoMOD129.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CursoMOD129.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyView()
+        {
+            ViewData["AuthorName"] = "Lyudmyla";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MyView(string author)
+        {
+            ViewData["AuthorName"] = "author";
+            return View();
+        }
+
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
