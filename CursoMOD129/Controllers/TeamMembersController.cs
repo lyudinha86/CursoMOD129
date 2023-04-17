78b930c846433b29e15ad677f15b7f17aed84b61
lyudinha86 <lyudinha712@gmail.com>
2023-04-17 18:49:42 +0100

    TeamMembers create

--- CursoMOD129/Controllers/TeamMembersController.cs
+++ CursoMOD129/Controllers/TeamMembersController.cs
@@ -1,6 +1,8 @@
 ﻿using CursoMOD129.Data;
+using CursoMOD129.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoMOD129.Controllers
 {
@@ -11,17 +13,37 @@ namespace CursoMOD129.Controllers
         {
             _context=context;
         }
+
         public IActionResult Index()
         {
-            return View();
-        }
+            var teamMembers = _context
+                .TeamMembers
+                .Include(tm => tm.WorkRole)
+                .ToList();
 
+            return View(teamMembers);
+        }
         //Get:TeamMembers/Create
 
         public IActionResult Create()
         {
+
             ViewData["WorkRoleID"] = new SelectList(_context.WorkRoles, "ID", "Name");
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TeamMember newTeamMember ) 
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(newTeamMember);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newTeamMember);
+
         }
     }
 }
--- CursoMOD129/Models/TeamMember.cs
+++ CursoMOD129/Models/TeamMember.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace CursoMOD129.Models
 {
@@ -8,7 +9,7 @@ namespace CursoMOD129.Models
         public int ID { get; set; }
 
         [Display(Name="Fullname")]
-        [StringLength(255)]
+        [StringLength(255, MinimumLength = 10)]
         [Required]
         public string Name { get; set; }
 
@@ -17,29 +18,32 @@ namespace CursoMOD129.Models
         [Required]
         public DateTime Birthday { get; set; }
 
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 5)]
         [Required]
         public string Address { get; set; }
 
 
         [Display(Name = "Zip Code")]
-        [StringLength(20)]
+        [StringLength(20, MinimumLength = 5)]
         [Required]
         public string ZipCode { get; set; }
 
 
 
-        [StringLength(50)]
+        [StringLength(50, MinimumLength = 1)]
         [Required]
         public string City { get; set; }
 
-        [StringLength(255)]
+        [StringLength(255, MinimumLength =8)]
         [Required]
         public string NIF { get; set; }
 
+       
+        [Display(Name = "Work Role")]
         [Required]
         public int WorkRoleID { get; set; }
 
+        [ValidateNever]
         public WorkRole WorkRole{ get; set; }
 
        
